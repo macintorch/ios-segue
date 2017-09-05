be7271eda889eef52664715d4786b77d74459008
Ainor Syahrizal <macintorch@gmail.com>
2017-09-05 16:06:45 +0800

    setup tableview

--- Segues/ViewController.swift
+++ Segues/ViewController.swift
@@ -8,7 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 4
+    }
+    
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = "Row \(indexPath.row)"
+        
+        return cell
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
